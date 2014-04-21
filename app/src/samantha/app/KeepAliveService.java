4a1d536e7c4f0b3719ab4f47154273174ecf8c60
ssumit <singhsumitbit@gmail.com>
2014-04-21 12:05:50 +0530

    adds sticky service for app keep alive

--- app/src/samantha/app/KeepAliveService.java
+++ app/src/samantha/app/KeepAliveService.java
@@ -1,4 +1,34 @@
 package samantha.app;
 
-public class KeepAliveService {
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class KeepAliveService extends Service
+{
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId)
+    {
+        return Service.START_STICKY;
+    }
+
+    @Override
+    public IBinder onBind(Intent intent)
+    {
+        return null;
+    }
+
+    @Override
+    public void onCreate()
+    {
+        super.onCreate();
+    }
+
+    @Override
+    public void onDestroy()
+    {
+        super.onDestroy();
+    }
 }
+
